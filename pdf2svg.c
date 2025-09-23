ff0709ec88708a303390ee06987e272b64d35177
wangjiezhe <wangjiezhe@gmail.com>
2025-09-23 23:47:06 +0800

    Use white background

--- pdf2svg.c
+++ pdf2svg.c
@@ -67,6 +67,11 @@ int convertPage(PopplerPage *page, const char* svgFilename)
 	cairo_svg_surface_set_document_unit(surface, CAIRO_SVG_UNIT_PT);
 	drawcontext = cairo_create(surface);
 
+	// Draw white background
+	cairo_set_source_rgb(drawcontext, 1.0, 1.0, 1.0); // white
+	cairo_rectangle(drawcontext, 0, 0, width, height);
+	cairo_fill(drawcontext);
+
 	// Render the PDF file into the SVG file
 	poppler_page_render_for_printing(page, drawcontext);
 	cairo_show_page(drawcontext);
